6965e8de7abe6e7782a953eb30d717e6a14b0b40
isabelladestefano <isabella.destefano@gmail.com>
2021-11-01 20:54:22 -0700

    fixed plot

--- results/slope_analysis.R
+++ results/slope_analysis.R
@@ -19,15 +19,15 @@ z_chain_data_exp1  = chain_data_exp1%>%
   mutate(z_error = (m-error)/s ) %>%
   group_by(seedID,setID, subject)%>%
   mutate(z_relative_pos = cumsum(z_error))%>%
-  mutate(experiment = "exp1") %>%
-  mutate(subject = factor(subject), seedID = as.character(seedID))%>%
-  filter(seed %in% seeds_exp3$seed)
+  mutate(subject = factor(subject), seedID = as.character(seedID))
+  #filter(seed %in% seeds_exp3$seed)
  
 slopes_exp1 = z_chain_data_exp1%>%
   select(subject,seedID, z_error, z_relative_pos, iteration, seed) %>% 
   group_by(subject, seedID, setID,seed) %>%
   do(slope = coef(lm(z_relative_pos~0+iteration, data=.))) %>%
-  unnest(cols = slope)
+  unnest(cols = slope) %>%
+  mutate(experiment = "exp1") 
 
 
 sub_error_summary_exp2 = test_data_exp2 %>% 
@@ -40,15 +40,15 @@ z_chain_data_exp2 = chain_data_exp2%>%
   mutate(z_error = (m-error)/s ) %>%
   group_by(seedID,setID, subject)%>%
   mutate(z_relative_pos = cumsum(z_error)) %>%
-  mutate(experiment = "exp2") %>%
-  mutate(subject = factor(subject), seedID = as.character(seedID))%>%
-  filter(seed %in% seeds_exp3$seed)
+  mutate(subject = factor(subject), seedID = as.character(seedID))
+  #filter(seed %in% seeds_exp3$seed)
 
 slopes_exp2= z_chain_data_exp2%>%
   select(subject,seedID, z_error, z_relative_pos, iteration, seed) %>% 
   group_by(subject, seedID, setID,seed) %>%
   do(slope = coef(lm(z_relative_pos~0+iteration, data=.))) %>%
-  unnest(cols = slope) 
+  unnest(cols = slope) %>%
+mutate(experiment = "exp2") 
 
 
 sub_error_summary_exp3 = test_data_exp3 %>% 
@@ -61,22 +61,31 @@ z_chain_data_exp3 = chain_data_exp3%>%
   mutate(z_error = (m-error)/s ) %>%
   group_by(seedID,setID, subject)%>%
   mutate(z_relative_pos = cumsum(z_error))%>%
-  mutate(experiment = "exp3")%>%
-  mutate(subject = factor(subject), seedID = as.character(seedID))%>%
-  filter(seed %in% seeds_exp3$seed)
+  mutate(subject = factor(subject), seedID = as.character(seedID))
+  #filter(seed %in% seeds_exp3$seed)
 
 
 slopes_exp3 = z_chain_data_exp3%>% 
   select(subject,seedID, z_error, z_relative_pos, iteration, seed) %>% 
   group_by(subject, seedID, setID, seed) %>%
   do(slope = coef(lm(z_relative_pos~0+iteration, data=.))) %>%
-  unnest(cols = slope) 
+  unnest(cols = slope) %>%
+  mutate(experiment = "exp3")
 
 bind_rows(slopes_exp1, slopes_exp2, slopes_exp3)%>%
+  filter(seed %in% c(18,90,162,234,306))%>%
+  mutate(setID = case_when(experiment == "exp1" ~ "exp1",
+                           setID == 1 ~"+20",
+                           setID == 2 ~ "-20"))%>%
+  mutate(seed = factor(seed))%>%
   ggplot(aes(x = slope, fill= setID))+
-  geom_histogram(position = "identity", alpha = .5, binwidth = 0.05)+
+  geom_histogram(position = "identity", alpha = .3, binwidth = 0.05)+
   geom_vline(xintercept = 0)+
   facet_grid(experiment~seed)
+
+
+
+slopes_exp2
 
 
 ggplot(z_chain_data_exp1)+
@@ -98,6 +107,8 @@ bind_rows(z_chain_data_exp1, z_chain_data_exp2, z_chain_data_exp3) %>%
   theme(panel.grid.minor.x = element_blank())+
   labs(y="Relative position", x="Iteration") +
   facet_grid(seed~.)
+
+
 
   
 
